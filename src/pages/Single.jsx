fd8f1afe28ac70d2ff613344cbe23777761e46aa
Tylianthuah <tylian15@gmail.com>
2023-02-25 00:39:02 +0530

    Single, Home and Write page compledet(Frontend)

--- src/pages/Single.jsx
+++ src/pages/Single.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import Menu from "../components/Menu";
 
 const Single = () => {
   return (
-    <div>Single</div>
-  )
-}
+    <div className="single">
+      <div className="content">
+        <img src="https://images.pexels.com/photos/13462177/pexels-photo-13462177.jpeg" />
+        <div className="user">
+          <img src="https://images.pexels.com/photos/13813505/pexels-photo-13813505.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+          <div className="info">
+            <span>Tylian</span>
+            <p>Posted 2 days ago</p>
+          </div>
+          <div className="edit">
+            <Link to={`/write?edit=2`}>
+              <img src={Edit} />
+            </Link>
+            <img src={Delete} />
+          </div>
+        </div>
 
-export default Single
+        <h1>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </h1>
+        <p>
+          Contrary to popular belief, Lorem Ipsum is not simply random text. It
+          has roots in a piece of classical Latin literature from 45 BC, making
+          it over 2000 years old. Richard McClintock, a Latin professor at
+          Hampden-Sydney College in Virginia, looked up one of the more obscure
+          Latin words, consectetur, from a Lorem Ipsum passage, and going
+          through the cites of the word in classical literature, discovered the
+          undoubtable source. Lorem Ipsum comes from sections 1.10.32 and
+          1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
+          Evil) by Cicero, written in 45 BC. This book is a treatise on the
+          theory of ethics, very popular during the Renaissance. 
+          <br />
+          <br />
+          The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in
+          section 1.10.32. The standard chunk of Lorem Ipsum used since the
+          1500s is reproduced below for those interested. Sections 1.10.32 and
+          1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also
+          reproduced in their exact original form, accompanied by English
+          versions from the 1914 translation by H. Rackham.
+          <br />
+          <br />
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </p>
+      </div>
+      <div className="menu">
+        <Menu />
+      </div>
+    </div>
+  );
+};
+
+export default Single;
